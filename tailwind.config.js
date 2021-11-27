97f2a4ad83cb8dcf85d5505e227c10ebd9e6a691
eken-leasify <andreas.ek@leasify.se>
2021-11-27 15:26:31 +0100

    After Tall Forms Installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,15 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+// const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-    variants: {
-        extend: {
-            backgroundColor: ['active'],
-        }
-    },
-    purge: {
-        content: [
-            './app/**/*.php',
-            './resources/**/*.html',
-            './resources/**/*.js',
-            './resources/**/*.jsx',
-            './resources/**/*.ts',
-            './resources/**/*.tsx',
-            './resources/**/*.php',
-            './resources/**/*.vue',
-            './resources/**/*.twig',
-        ],
-        options: {
-            defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
-            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
-        },
-    },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
+    mode: 'jit', //https://tailwindcss.com/docs/just-in-time-mode#enabling-jit-mode
+    presets: [
+        // the preset merges, fonts, variants, plugins, colors and purge content
+        require('./vendor/tanthammar/tall-forms/resources/stubs/tailwindcss/2.0/tall-forms-preset.js'),
     ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: [],
 };
